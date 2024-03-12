1453057445287fc829a0190fe98aa4a0d7ccc84b
DiegoCmoreira <diego.moreira@sptech.school>
2024-03-12 14:15:48 -0300

    Inicio de lógica para filtro de próximo/histórico

--- app/src/main/java/com/example/vitaeapp/MainActivity.kt
+++ app/src/main/java/com/example/vitaeapp/MainActivity.kt
@@ -1,45 +1,31 @@
 package com.example.vitaeapp
 
 import android.os.Bundle
-import android.print.PrintAttributes.Margins
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.Button
-import androidx.compose.material3.DropdownMenu
-import androidx.compose.material3.DropdownMenuItem
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateListOf
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.snapshots.SnapshotStateList
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.paint
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.example.vitaeapp.ui.theme.VitaeAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -61,7 +47,22 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Tela(name: String, modifier: Modifier = Modifier) {
+    val listaHistorico = remember {
+        mutableStateListOf(
+            Historico(id = 5 , data = "2024/06/26", hora = "10:30", hemocentro = "Hospital Teste"),
+            Historico(id = 4 , data = "2024/05/14", hora = "10:00", hemocentro = "Hospital Teste"),
+            Historico(id = 3 , data = "2024/02/27", hora = "10:30", hemocentro = "Hospital Exemplo"),
+            Historico(id = 2 , data = "2023/11/01", hora = "10:00", hemocentro = "Hospital Exemplo"),
+            Historico(id = 1 , data = "2023/09/06", hora = "10:30", hemocentro = "Hospital Exemplo"),
+        )
+    }
+
     Logo()
+    Column {
+        Proxima(lista = listaHistorico)
+        Text("HISTÓRICO")
+
+    }
     Menu()
 }
 
@@ -103,8 +104,54 @@ fun Menu() {
         }
         Spacer(modifier = Modifier.width(8.dp))
     }
+}
 
+@Composable
+fun Proxima(lista:List<Historico>){
 
+    var data:String = ""
+    var hora:String = ""
+    var hospital:String = ""
+
+    var hemocentro = lista.maxBy { it.id }
+
+    /*
+    lista.forEach { itemIdMaior ->
+        lista.forEach {
+            itemIdMenor ->
+            if (itemIdMaior.id > itemIdMenor.id){
+                data = itemIdMaior.data
+                hora = itemIdMaior.hora
+                hospital = itemIdMaior.hemocentro
+            }
+        }
+    }
+     */
+
+    Column(
+        Modifier.
+        padding(30.dp, 50.dp)
+    ) {
+        Text(
+            "PRÓXIMA DOAÇÃO",
+            Modifier.padding(0.dp, 0.dp, 0.dp, 10.dp)
+            )
+        Column {
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                Image(
+                    painter = painterResource(id = R.mipmap.hospital),
+                    contentDescription = "Doação",
+                    modifier = Modifier.size(55.dp)
+                )
+
+                Column {
+                    Text("Doação: n° ")
+                    Text("Data: ${hemocentro.data} - Hora: ${hemocentro.hora}")
+                    Text("Hemocentro: ${hemocentro.hemocentro}")
+                }
+            }
+        }
+    }
 }
 
 
