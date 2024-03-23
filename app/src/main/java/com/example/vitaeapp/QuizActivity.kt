1b41bba00eeda5cfb0dc03dd4b1bf96cb022aebf
DiegoCmoreira <diego.moreira@sptech.school>
2024-03-23 16:13:30 -0300

    FEAT/ Lógica para alteração de perguntas

--- app/src/main/java/com/example/vitaeapp/QuizActivity.kt
+++ app/src/main/java/com/example/vitaeapp/QuizActivity.kt
@@ -1,5 +1,6 @@
 package com.example.vitaeapp
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -17,10 +18,11 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
@@ -55,74 +57,381 @@ class QuizActivity : ComponentActivity() {
 fun TelaQuiz() {
     Logo()
     Column {
-
+        Questionario()
     }
     Menu()
 }
 
 @Composable
 fun Questionario() {
-    val pergunta = remember {
-        mutableStateListOf(
-            "Informe seu peso:",
-            "Informe sua altura:",
-            "Fez alguma tatuagem nos últimos 6 meses?",
-            "Teve algum tipo de relação sexual recentemente?",
-            "Sente algum desconforto, ou dor na barriga?",
-            "Faz ou fez uso de algum tipo de medicamento?",
-            "Tem algum tipo de DST?",
-            "Tomou alguma vacina contra a COVID-19 recentimente? :",
-        )
-    }
+    val perguntas = remember { mutableStateOf(1) }
 
     Text(
         "QUIZ DE APTIDÃO",
         style = TextStyle(fontFamily = fontFamilyRowdiesBold)
     )
     Column {
+        when (perguntas.value) {
+            1 -> perguntaPeso()
+            2 -> perguntaAltura()
+            3 -> perguntaTatuagem()
+            4 -> perguntaRelacao()
+            5 -> perguntaDesconforto()
+            6 -> perguntaMedicamento()
+            7 -> perguntaDst()
+            8 -> perguntaMedicamento()
+        }
 
+        if (perguntas.value < 8) {
+            BotaoAvancar {
+                perguntas.value++
+            }
+        }
+
+        if (perguntas.value > 1) {
+            BotaoVoltar {
+                perguntas.value--
+            }
+        }
     }
 }
 
 @Composable
-fun BotaoQuiz(valor:String){
-    Box(  modifier = Modifier
-        .fillMaxWidth()
-        .height(120.dp)
-        .padding(top = 10.dp)
-        .background(Color.Transparent, shape = RoundedCornerShape(16.dp)),
+fun BotaoAvancar(onClick: () -> Unit) {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(120.dp)
+            .padding(top = 10.dp)
+            .background(Color.Transparent, shape = RoundedCornerShape(16.dp)),
         contentAlignment = Alignment.Center,
 
         ) {
 
-        Row(
-
+        IconButton(
             modifier = Modifier
-                .width(200.dp)
-                .height(45.dp)
-                .background(color = colorResource(id = R.color.vermelho_rosado), shape = RoundedCornerShape(16.dp))
-                .border(
-                    color = Color.Black,
-                    width = 2.dp,
-                    shape = RoundedCornerShape(16.dp)
-                ),
-            horizontalArrangement = Arrangement.SpaceAround,
-            verticalAlignment = Alignment.CenterVertically,
-
-            ){
-
-            Text(
-                valor, fontSize = 18.sp, fontFamily = fontRobotoBold
-            )
-            Image(
-                painter = painterResource(id = R.mipmap.seta_direita),
-                contentDescription = null,
+                .width(210.dp)
+                .height(45.dp),
+            onClick = { }
+        ) {
+            Row(
                 modifier = Modifier
-                    .size(45.dp)
-            )
+                    .width(200.dp)
+                    .height(45.dp)
+                    .background(
+                        color = colorResource(id = R.color.vermelho_rosado),
+                        shape = RoundedCornerShape(16.dp)
+                    )
+                    .border(
+                        color = Color.Black,
+                        width = 2.dp,
+                        shape = RoundedCornerShape(16.dp)
+                    ),
+                horizontalArrangement = Arrangement.SpaceAround,
+                verticalAlignment = Alignment.CenterVertically,
+
+                ) {
+
+                Text(
+                    "Avançar", fontSize = 18.sp, fontFamily = fontRobotoBold
+                )
+                Image(
+                    painter = painterResource(id = R.mipmap.seta_direita),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .size(45.dp)
+                )
+            }
         }
     }
 }
+
+@Composable
+fun BotaoVoltar(onClick: () -> Unit) {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(120.dp)
+            .padding(top = 10.dp)
+            .background(Color.Transparent, shape = RoundedCornerShape(16.dp)),
+        contentAlignment = Alignment.Center,
+
+        ) {
+
+        IconButton(
+            modifier = Modifier
+                .width(210.dp)
+                .height(45.dp),
+            onClick = { }
+        ) {
+            Row(
+                modifier = Modifier
+                    .width(200.dp)
+                    .height(45.dp)
+                    .background(
+                        color = colorResource(id = R.color.vermelho_rosado),
+                        shape = RoundedCornerShape(16.dp)
+                    )
+                    .border(
+                        color = Color.Black,
+                        width = 2.dp,
+                        shape = RoundedCornerShape(16.dp)
+                    ),
+                horizontalArrangement = Arrangement.SpaceAround,
+                verticalAlignment = Alignment.CenterVertically,
+
+                ) {
+
+                Text(
+                    "Voltar", fontSize = 18.sp, fontFamily = fontRobotoBold
+                )
+                Image(
+                    painter = painterResource(id = R.mipmap.seta_direita),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .size(45.dp)
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun BotaoSim() {
+    val respostaSim = remember { mutableStateOf(true) }
+
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(120.dp)
+            .padding(top = 10.dp)
+            .background(Color.Transparent, shape = RoundedCornerShape(16.dp)),
+        contentAlignment = Alignment.Center,
+
+        ) {
+
+        IconButton(
+            modifier = Modifier
+                .width(210.dp)
+                .height(45.dp),
+            onClick = { }
+        ) {
+            Row(
+                modifier = Modifier
+                    .width(200.dp)
+                    .height(45.dp)
+                    .background(
+                        color = colorResource(id = R.color.vermelho_rosado),
+                        shape = RoundedCornerShape(16.dp)
+                    )
+                    .border(
+                        color = Color.Black,
+                        width = 2.dp,
+                        shape = RoundedCornerShape(16.dp)
+                    ),
+                horizontalArrangement = Arrangement.SpaceAround,
+                verticalAlignment = Alignment.CenterVertically,
+
+                ) {
+
+                Text(
+                    "Sim", fontSize = 18.sp, fontFamily = fontRobotoBold
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun BotaoNao() {
+    val respostaNao = remember { mutableStateOf(false) }
+
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(120.dp)
+            .padding(top = 10.dp)
+            .background(Color.Transparent, shape = RoundedCornerShape(16.dp)),
+        contentAlignment = Alignment.Center,
+
+        ) {
+
+        IconButton(
+            modifier = Modifier
+                .width(210.dp)
+                .height(45.dp),
+            onClick = { }
+        ) {
+            Row(
+                modifier = Modifier
+                    .width(200.dp)
+                    .height(45.dp)
+                    .background(
+                        color = colorResource(id = R.color.vermelho_rosado),
+                        shape = RoundedCornerShape(16.dp)
+                    )
+                    .border(
+                        color = Color.Black,
+                        width = 2.dp,
+                        shape = RoundedCornerShape(16.dp)
+                    ),
+                horizontalArrangement = Arrangement.SpaceAround,
+                verticalAlignment = Alignment.CenterVertically,
+
+                ) {
+
+                Text(
+                    "Não", fontSize = 18.sp, fontFamily = fontRobotoBold
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun perguntaAltura() {
+    val altura = remember { mutableStateOf("") }
+
+    Column {
+        Text(
+            "Informe sua altura:",
+            style = TextStyle(fontFamily = fontRobotoRegular)
+        )
+        TextField(
+            value = altura.value,
+            onValueChange = { altura.value = it },
+            singleLine = true, // Define o TextField como uma única linha
+            modifier = Modifier.fillMaxWidth()
+        )
+        Row {
+            Text("1/8")
+        }
+    }
+}
+
+@Composable
+fun perguntaPeso() {
+    val peso = remember { mutableStateOf("") }
+
+    Column {
+        Text(
+            "Informe seu peso:",
+            style = TextStyle(fontFamily = fontRobotoRegular)
+        )
+        TextField(
+            value = peso.value,
+            onValueChange = { peso.value = it },
+            singleLine = true, // Define o TextField como uma única linha
+            modifier = Modifier.fillMaxWidth()
+        )
+        Row {
+            Text("2/8")
+        }
+    }
+}
+
+@Composable
+fun perguntaTatuagem() {
+    Column {
+        Text(
+            "Fez alguma tatuagem nos últimos 6 meses?",
+            style = TextStyle(fontFamily = fontRobotoRegular)
+        )
+        Row {
+            BotaoNao()
+            BotaoSim()
+        }
+        Row {
+            Text("3/8")
+        }
+    }
+}
+
+@Composable
+fun perguntaRelacao() {
+    Column {
+        Text(
+            "Teve algum tipo de relação sexual recentemente?",
+            style = TextStyle(fontFamily = fontRobotoRegular)
+        )
+        Row {
+            BotaoNao()
+            BotaoSim()
+        }
+        Row {
+            Text("4/8")
+        }
+    }
+}
+
+@Composable
+fun perguntaDesconforto() {
+    Column {
+        Text(
+            "Sente algum desconforto, ou dor na barriga?",
+            style = TextStyle(fontFamily = fontRobotoRegular)
+        )
+        Row {
+            BotaoNao()
+            BotaoSim()
+        }
+        Row {
+            Text("5/8")
+        }
+    }
+}
+
+@Composable
+fun perguntaMedicamento() {
+    Column {
+        Text(
+            "Faz ou fez uso de algum tipo de medicamento?",
+            style = TextStyle(fontFamily = fontRobotoRegular)
+        )
+        Row {
+            BotaoNao()
+            BotaoSim()
+        }
+        Row {
+            Text("6/8")
+        }
+    }
+}
+
+@Composable
+fun perguntaDst() {
+    Column {
+        Text(
+            "Tem algum tipo de DST?",
+            style = TextStyle(fontFamily = fontRobotoRegular)
+        )
+        Row {
+            BotaoNao()
+            BotaoSim()
+        }
+        Row {
+            Text("7/8")
+        }
+    }
+}
+
+@Composable
+fun perguntaVacina() {
+    Column {
+        Text(
+            "Tomou alguma vacina contra a COVID-19 recentimente? :",
+            style = TextStyle(fontFamily = fontRobotoRegular)
+        )
+        Row {
+            BotaoNao()
+            BotaoSim()
+        }
+        Row {
+            Text("8/8")
+        }
+    }
+}
+
 
 @Preview(showBackground = true)
 @Composable
